35dbc0c06b8739d2e5b0b8b94f2b317af02ad0c2
easoncat <734632655@qq.com>
2024-08-01 18:34:01 +0800

    add mock data

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,11 +3,22 @@ import { AppService } from './app.service';
 
 @Controller('api')
 export class AppController {
+  // 这种在 constructor 中直接定义私有变量的方式，其实是Ts的简写形式
+  // 等价于
+  // constructor(db: DatabaseService) {
+  //   this.databaseService = db
+  // }
+  // constructor(private databaseService: DatabaseService)
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('chartData')
+  getChartData(): Object {
+    return this.appService.getChartData();
   }
 
   @Get('login')
--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       retryAttempts:10,//重试连接数据库的次数
       autoLoadEntities:true, //如果为true,将自动加载实体 forFeature()方法注册的每个实体都将自动添加到配置对象的实体数组中
   })],
+  // imports: [CustomeuserModule, ListModule, UploadModule],
   controllers: [AppController],
   providers: [AppService],
 })
